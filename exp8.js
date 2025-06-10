baba568294cdf6d901a144b1f22c972ddee0ff14
Júlio <julinhoalencar30@gmail.com>
2025-06-10 11:03:22 -0300

    código

--- exp8.js
+++ exp8.js
@@ -1,1 +1,14 @@
 const prompt = require("prompt-sync")();
+
+let n = [];
+let loop = true
+
+while(loop){
+    let entrada = parseInt(prompt("Digite seus números: "))
+    if(entrada === 0){
+        loop = false
+    }else{
+        n.push(entrada)
+    }
+}
+console.log("Números digitados: ", n)
